4f8b1d3a6d546bbdaeb3a9d58700747a39d9eb31
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-17 17:34:09 -0300

    feat: create migration blog_posts

---
+++ src/migrations/20230717202623-create-blog-posts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts',{
+      id:{ type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
+      title: {type: Sequelize.STRING},
+      content: {type: Sequelize.STRING},
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+        }
+      },
+      published: {type: Sequelize.DATE},
+      updated: {type: Sequelize.DATE},
+    },{
+      underscored: true,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts')
+  }
+};
