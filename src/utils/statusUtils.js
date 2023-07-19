6b21b5609c4062aa32196742d9d27fb8d916a929
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-18 22:46:26 -0300

    feat: create statusUtils

---
+++ src/utils/statusUtils.js
@@ -0,0 +1,10 @@
+const badRequest = 400;
+
+const invalidFields = { message: 'Invalid fields' };
+const missingField = { message: 'Some required fields are missing' };
+
+module.exports = {
+  invalidFields,
+  missingField,
+  badRequest,
+};
