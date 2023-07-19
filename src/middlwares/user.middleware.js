fc3ceea993a66e5817172aa18f36502ce5a1544c
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-19 16:45:56 -0300

    feat: create user middleware

---
+++ src/middlwares/user.middleware.js
@@ -0,0 +1,29 @@
+const { statusBadRequest, 
+  displayNameInvalid, 
+  emailInvalid,
+  passwordInvalid, 
+  statusConflict,
+  userRegistred } = require('../utils/statusUtils');
+const { invalidDisplayName,
+  invalidEmail, 
+  invalidPassword,
+  existingEmail } = require('../utils/validationUtils');
+
+const userMiddleware = (req, res, next) => {
+  const { displayName, email, password } = req.body;
+  if (invalidDisplayName(displayName)) {
+    return res.status(statusBadRequest).json(displayNameInvalid);
+  }
+  if (invalidEmail(email)) {
+    return res.status(statusBadRequest).json(emailInvalid);
+  }
+  if (invalidPassword(password)) {
+    return res.status(statusBadRequest).json(passwordInvalid);
+  }
+  if (existingEmail(email)) {
+    return res.status(statusConflict).json(userRegistred);
+  }
+  return next();
+};
+
+module.exports = userMiddleware;
