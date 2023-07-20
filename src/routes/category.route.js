3a762d81780378c017c9c9019f02025578135918
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-20 17:04:56 -0300

    feat: create route /categories

---
+++ src/routes/category.route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const categoryRoute = express.Router();
+
+categoryRoute.post('/');
+
+module.exports = categoryRoute;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const loginRoute = require('./login.route');
 const userRoute = require('./user.route');
+const categoryRoute = require('./category.route');
+const authMiddleware = require('../middlwares/auth.middleware');
 
 const routes = express.Router();
 routes.use('/login', loginRoute);
 routes.use('/user', userRoute);
+routes.user('/categories', categoryRoute, authMiddleware);
 
 module.exports = routes;
