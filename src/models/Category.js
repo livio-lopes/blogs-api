1f02e9389dbb30b259c04bf593f929280223d9e5
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-20 16:59:50 -0300

    feat: create model Category

---
+++ src/models/Category.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category',{
+    id:{type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: DataTypes.STRING,
+  },{
+    timestamps: false,
+    tableName: 'categories'
+  })
+  return Category;
+}
