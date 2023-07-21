406ee9810337c6cfdd39b68a91b780a868952e2f
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-21 00:04:59 -0300

    feat: create endpoint GET /categories

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middlwares/auth.middleware');
 const categoryMiddleware = require('../middlwares/category.middleware');
-const { addCategory } = require('../controller/category.controller');
+const { addCategory, getAllCategories } = require('../controller/category.controller');
 
 const categoryRoute = express.Router();
 
+categoryRoute.get('/', authMiddleware, getAllCategories);
 categoryRoute.post('/', authMiddleware, categoryMiddleware, addCategory);
 
 module.exports = categoryRoute;
