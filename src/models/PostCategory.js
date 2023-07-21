02a72fd5c84bb3806a2475537974ec8939f5d517
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-21 00:36:38 -0300

    feat: create association categories has many blogposts and blogposts has many categories

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -7,6 +7,16 @@ module.exports = (sequelize, DataTypes) =>{
     timestamps: false,
     tableName: 'users'
   })
-
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, { as: 'blogPosts',
+    through: PostCategory,
+    foreignKey: 'categoryId',
+    otherKey: 'postId' });
+    models.BlogPost.belongsToMany(models.Category, { as: 'categories',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId' });
+  };
+  
   return PostCategory;
 }
