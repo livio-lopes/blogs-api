b5eb4990e7e2dd86e92697b328d2582f2ef96a9d
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-20 23:58:20 -0300

    feat: verify two types token

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -10,8 +10,11 @@ const extractToken = (bearerToken) => bearerToken.split(' ')[1];
 
 const createToken = (info) => jwt.sign(info, cipher, config);
 
-const verifyToken = (bearerToken) => {
-  const token = extractToken(bearerToken);
+const verifyToken = (token) => {
+  if (extractToken(token)) {
+    const bearerToken = extractToken(token);
+    return jwt.verify(bearerToken, cipher);
+  }
   return jwt.verify(token, cipher);
 };
 
