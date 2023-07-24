b3da887e3eb940544003489dc274707d39932254
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-24 18:38:43 -0300

    feat: create updatePost on service

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -5,55 +5,59 @@ const config = require('../config/config');
 const env = process.env.NODE_EVN || 'development';
 const sequelize = new Sequelize(config[env]);
 
+const includeConfig = {
+  include: [
+    {
+      model: User,
+      as: 'user',
+      attributes: { exclude: ['password'] },
+    },
+    {
+      model: Category,
+      as: 'categories',
+      through: { attributes: [] },
+    },
+  ],
+};
+
 const createPost = async (infoPost) => {
   const { title, content, userId, categoryIds } = infoPost;
   console.log('service', infoPost);
   const resultTransaction = await sequelize.transaction(async (t) => {
     const newPost = await BlogPost.create(
-      { title, content, userId }, 
+      { title, content, userId },
       { transaction: t },
-    );  
+    );
     const listPostCategories = categoryIds.map((id) => ({
-        postId: newPost.id,
-        categoryId: id,
-      }));
-     await PostCategory.bulkCreate(listPostCategories, { transaction: t });
+      postId: newPost.id,
+      categoryId: id,
+    }));
+    await PostCategory.bulkCreate(listPostCategories, { transaction: t });
     return newPost;
   });
   return resultTransaction;
 };
 
 const getAllPost = async () => {
-  const allPosts = await BlogPost.findAll(
-    { include: [
-      { model: User,
-         as: 'user', 
-         attributes: { exclude: ['password'] }, 
-      },
-      {
-        model: Category,
-        as: 'categories',
-        through: { attributes: [] },
-      },
-        ] },
-);
-
+  const allPosts = await BlogPost.findAll(includeConfig);
   return allPosts;
 };
 
-  const getPostById = async (id) => {
-    const postById = await BlogPost.findByPk(Number(id), { include: [
-      { model: User,
-         as: 'user', 
-         attributes: { exclude: ['password'] }, 
-      },
-      {
-        model: Category,
-        as: 'categories',
-        through: { attributes: [] },
-      },
-        ] });
-    return postById;
-  };
+const getPostById = async (id) => {
+  const postById = await BlogPost.findByPk(Number(id), includeConfig);
+  return postById;
+};
+const updatePost = async ({ id, title, content }) => {
+  const updatedPost = await BlogPost.update(
+    { title, content },
+    { where: { id } },
+  );
+  return updatedPost;
+};
 
-module.exports = { createPost, getAllPost, getPostById };
+module.exports = {
+  createPost,
+  getAllPost,
+  getPostById,
+  updatePost,
+};
