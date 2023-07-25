6a7cce08980f0d94d250dc083a1d12a113922afc
Lívio Lopes <antonio.lvio760@gmail.com>
2023-07-25 09:57:11 -0300

    feat: create endpoint DELETE /user/me

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userMiddleware = require('../middlwares/user.middleware');
-const { addUser, getAllUsers, getUserById } = require('../controller/user.controller');
+const { addUser, getAllUsers, getUserById, deleteUser } = require('../controller/user.controller');
 const authMiddleware = require('../middlwares/auth.middleware');
 
 const userRoute = express.Router();
@@ -8,6 +8,6 @@ const userRoute = express.Router();
 userRoute.get('/', authMiddleware, getAllUsers);
 userRoute.get('/:id', authMiddleware, getUserById);
 userRoute.post('/', userMiddleware, addUser);
-userRoute.delete('/me', authMiddleware);
+userRoute.delete('/me', authMiddleware, deleteUser);
 
 module.exports = userRoute;
